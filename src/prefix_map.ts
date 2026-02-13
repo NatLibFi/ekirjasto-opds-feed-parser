1cae165abd9093b9d74c62cafd55f415a33c4f64
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2026-02-13 15:35:00 +0200

    EKIRJASTO-714 Add comments to namespace parsing

--- src/prefix_map.ts
+++ src/prefix_map.ts
@@ -1,3 +1,7 @@
+// Define an interface PrefixMap
+// that maps strings to strings.
+// Usestypescript index signature,
+// map key can be any string
 interface PrefixMap {
   [index: string]: string;
 }
